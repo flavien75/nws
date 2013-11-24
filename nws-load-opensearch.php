4333a33ed0f304dc88422c18b15a6b84775113f6
Flavien <flavien@localhost.localdomain>
2013-11-24 18:17:08 +0100

    far from being finished

--- nws-load-opensearch.php
+++ nws-load-opensearch.php
@@ -9,14 +9,74 @@
 ini_set('display_errors', 'Off');
 include('nws-favicon.php');
 
+function get_opensearch_content($url) {
+    $html_page = file_get_contents($url);
+    if ($html_page === false) return false;
+    
+    $html_doc = new DOMDocument();
+    $html_doc->strictErrorChecking = FALSE;
+    @$html_doc->loadHTML($html_page);
+    $html_links = $html_doc->getElementsByTagName('link');
+    $link_opensearch = false;
+    foreach ($html_links as $link) {
+        $link_rel  = $link->getAttribute('rel');
+        $link_type = $link->getAttribute('type');
+        if (($link_rel == 'search') && ($link_type == 'application/opensearchdescription+xml')) {
+            $link_opensearch = $link->getAttribute('href');
+        }
+    }
+    if ($link_opensearch === false) {
+        echo "Can't find any link to OpenSearch descriptor<br />\n";
+        return false;
+    }
+    // check if link is absolute or relative
+    if (!((substr($link_opensearch, 0, strlen('http://')) == 'http://') || (substr($link_opensearch, 0, strlen('https://')) == 'https://'))) {
+        $url_parsed = parse_url($url);
+        if (isset($url_parsed['scheme']))
+            $link_opensearch = $url_parsed['scheme'].'://'.$url_parsed['host'].$link_opensearch;
+        else
+            $link_opensearch = 'http://'.$url_parsed['host'].$link_opensearch;
+    }
+    $xml_opensearch = file_get_contents($link_opensearch);
+    
+    if ($xml_opensearch === false) return false;
+
+    $opensearch_desc = simplexml_load_string($xml_opensearch);
+    if ($opensearch_desc === false) {
+        echo "Can't load XML<br />\n";
+        return false;
+    }
+    $result['title']    = $opensearch_desc->ShortName;  // always there
+    $result['full_desc']= $opensearch_desc->Description;// always there
+    foreach ($opensearch_desc->Url as $u) { // must appear one or more time
+        $u_attr = $u->attributes();
+        $type = $u_attr['type'];   // always there
+        if ($type == 'text/html') {
+            $result['html_template'] = $u_attr['template'];
+        }
+    }
+    if (isset($opensearch_desc->Image))     // sometime there
+        $result['favicon'] = $opensearch_desc->Image;
+}
+
 function gen_opensearch_div($url) {
+    $search_param = get_opensearch_content($url);
+    if ($search_param === false) return false;
+    
+    if (!isset($search_param['html_template'])) return false;
+    
+    if (!isset($search_param['favicon'])) {
+        $u = parse_url($search_param['html_template']);
+        $search_param['favicon'] = get_favicon($u['host']); // not using cached version since we wil cache the result
+    }
+    
     // !!! TEMPORARY CODE !!!
-    $favicon = get_favicon($url); // not using cached version since we wil cache the result
     $div_content = '
 <div class="search" title ="'.$url.'">
+    <!-- '.$search_param['html_template'].' -->
     <div class="feedTitle">
         <span class="favicon">
-            <a href="'.$url.'"><img src="'.$favicon.'" /></a>&nbsp;<a href="'.$url.'" title="'.$url.'">'.$url.'</a>
+            <a href="'.$url.'"><img src="'.$search_param['favicon'].'" /></a>&nbsp;<a href="'.$url.'" title="'.$search_param['title'].'">'.$search_param['title'].'</a>
         </span>
     </div>
     <form action="http://fr.wikipedia.org/w/index.php?title=Sp%C3%A9cial:Recherche" method="get">
@@ -40,7 +100,10 @@ function get_opensearch_div($url, $max_age) {
     }
     
     if (!$cache_ok) { // directory missing and unable to create it => abort cache feature
-        return gen_opensearch_div($url);
+        $age = 0;
+        $div_content = gen_opensearch_div($url);
+        if ($div_content === false) die("Load error");
+        return array($div_content, $age);
     }
 
     $u = parse_url($url);
@@ -58,13 +121,29 @@ function get_opensearch_div($url, $max_age) {
 
     if ($div_content === false) { // either unreadable cache file, or cache file is too old
         $div_content = gen_opensearch_div($url);
-        $fh = fopen($cache_file, 'w');
-        if ($fh !== false) {
-            fwrite($fh, $div_content);
-            fclose($fh);
+        $age = 0;
+        if ($div_content !== false) {
+            $fh = fopen($cache_file, 'w');
+            if ($fh !== false) {
+                fwrite($fh, $div_content);
+                fclose($fh);
+            }
         }
     }
-    return $div_content;
+    
+    if ($div_content === false) {
+        // can't get data from remote site
+        // fallback to previous version if it exists
+        if (file_exists($cache_file)) {
+            // ok, we have an (old) version in cache
+            $age = time() - filemtime($cache_file);
+            $div_content = file_get_contents($cache_file) or die("Load / read error (cache)");
+        } else {
+            die("Load / read error (cache)");
+        }
+        
+    }
+    return array($div_content, $age);
 }
 
 if (isset($_GET['age']))
@@ -72,6 +151,7 @@ if (isset($_GET['age']))
 else
     $max_age = 30*24*3600; // 30 days since it is not supposed to change
 
-echo get_opensearch_div($_GET['z'], $max_age);
+list($content, $content_age) = get_opensearch_div($_GET['z'], $max_age);
+echo $content;
 
 ?>
